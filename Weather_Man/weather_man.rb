831dcb84de511c0d16592368786cb4c00195611a
FaizDeccon <86602853+FaizDeccon@users.noreply.github.com>
2021-07-11 13:17:38 +0500

    Update weather_man.rb

--- Weather_Man/weather_man.rb
+++ Weather_Man/weather_man.rb
@@ -2,11 +2,12 @@
 
 require 'colorize'
 require_relative 'data_functions'
-Object.include(Tools)
+Object.include(Month)
+Object.include(Year)
 filename = "#{ARGV[2].split('/')[-1]}_#{ARGV[1].split('/')[0]}_"
-Tools.year_func(ARGV[2], filename) if ARGV[0] == '-e'
+Year.year_func(ARGV[2], filename) if ARGV[0] == '-e'
 monthnum = ARGV[1].split('/')[-1].to_i.to_s
-Tools.mon_func(ARGV[2], filename, monthnum) if ARGV[0] == '-a'
+Month.mon_func(ARGV[2], filename, monthnum) if ARGV[0] == '-a'
 year = ARGV[1].split('/')[0]
-Tools.print_lines(ARGV[2], filename, year, monthnum) if ARGV[0] == '-c' && ARGV[1].split('/')[-1].to_s[0] != '0'
-Tools.print_bonus(ARGV[2], filename, year, monthnum) if ARGV[0] == '-c' && ARGV[1].split('/')[-1].to_s[0] == '0'
+Month.print_lines(ARGV[2], filename, year, monthnum) if ARGV[0] == '-c' && ARGV[1].split('/')[-1].to_s[0] != '0'
+Month.print_bonus(ARGV[2], filename, year, monthnum) if ARGV[0] == '-c' && ARGV[1].split('/')[-1].to_s[0] == '0'
