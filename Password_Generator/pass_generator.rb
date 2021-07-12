a0833ff0cd8bbbcd74701e258f6b405b6388530f
FaizDeccon <86602853+FaizDeccon@users.noreply.github.com>
2021-07-12 11:45:01 +0500

    Update pass_generator.rb

--- Password_Generator/pass_generator.rb
+++ Password_Generator/pass_generator.rb
@@ -32,22 +32,22 @@ if alphabets == 'Y' && numbers == 'Y' && s_characters == 'Y'
   pass = Array.new(p_length / 4) { ('a'..'z').to_a[rand(26)] + ('A'..'Z').to_a[rand(26)] + ('0'..'9').to_a[rand(10)] + rand(33..47).chr }.join
   (p_length - pass.length).times { pass << ('0'..'9').to_a[rand(10)] }
 elsif alphabets == 'Y' && numbers == 'N' && s_characters == 'Y'
-  pass = Array.new(p_length / 4) { ('a'..'z').to_a[rand(26)] + ('A'..'Z').to_a[rand(26)] + rand(33..47).chr }.join
+  pass = Array.new(p_length / 3) { ('a'..'z').to_a[rand(26)] + ('A'..'Z').to_a[rand(26)] + rand(33..47).chr }.join
   (p_length - pass.length).times { pass << rand(33..47).chr }
 elsif alphabets == 'Y' && numbers == 'Y' && s_characters == 'N'
-  pass = Array.new(p_length / 4) { ('a'..'z').to_a[rand(26)] + ('A'..'Z').to_a[rand(26)] + ('0'..'9').to_a[rand(10)] }.join
+  pass = Array.new(p_length / 3) { ('a'..'z').to_a[rand(26)] + ('A'..'Z').to_a[rand(26)] + ('0'..'9').to_a[rand(10)] }.join
   (p_length - pass.length).times { pass << ('0'..'9').to_a[rand(10)] }
 elsif alphabets == 'Y' && numbers == 'N' && s_characters == 'N'
-  pass = Array.new(p_length / 4) { ('a'..'z').to_a[rand(26)] + ('A'..'Z').to_a[rand(26)] }.join
+  pass = Array.new(p_length / 2) { ('a'..'z').to_a[rand(26)] + ('A'..'Z').to_a[rand(26)] }.join
   (p_length - pass.length).times { pass << ('a'..'z').to_a[rand(26)] }
 elsif alphabets == 'N' && numbers == 'Y' && s_characters == 'Y'
-  pass = Array.new(p_length / 4) { ('0'..'9').to_a[rand(10)] + rand(33..47).chr }.join
+  pass = Array.new(p_length / 2) { ('0'..'9').to_a[rand(10)] + rand(33..47).chr }.join
   (p_length - pass.length).times { pass << rand(33..47).chr }
 elsif alphabets == 'N' && numbers == 'N' && s_characters == 'Y'
-  pass = Array.new(p_length / 4) { rand(33..47).chr }.join
+  pass = Array.new(p_length) { rand(33..47).chr }.join
   (p_length - pass.length).times { pass << rand(33..47).chr }
 elsif alphabets == 'N' && numbers == 'Y' && s_characters == 'N'
-  pass = Array.new(p_length / 4) { ('0'..'9').to_a[rand(10)] }.join
+  pass = Array.new(p_length) { ('0'..'9').to_a[rand(10)] }.join
   (p_length - pass.length).times { pass << ('0'..'9').to_a[rand(10)] }
 else
   puts 'All three fields cannot be empty'
